b07d3cbc9fc41b16c6adbfe19663d9662a7fb9a8
potgon <otgon.petru@gmail.com>
2025-05-07 08:28:50 +0200

    feat: migrate database to Neon

--- backend/src/main/resources/db/migration/V2__Indexes.sql
+++
@@ -1,4 +0,0 @@
--- Indexes for performance
-CREATE INDEX idx_transactions_periods ON transactions (period_id);
-CREATE INDEX idx_transactions_date ON transactions (date);
-CREATE INDEX idx_transactions_category ON transactions (category_id);
